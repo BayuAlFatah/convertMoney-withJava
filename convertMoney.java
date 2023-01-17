bca5340e5d6cc6c4105fd59b67176a2a571c1b45
BayuAlFatah <bayualfatahsetiawan5@gmail.com>
2023-01-17 22:32:42 +0700

    membuat file main

---
+++ convertMoney.java
@@ -0,0 +1,18 @@
+package PROJECT.konversiUang.baru;
+
+import java.util.Scanner;
+
+public class convertMoney {
+    public static void main(String[] args) {
+        Scanner inputUser = new Scanner(System.in);
+        float a;
+        char b;
+        var dollar = 0.000064;
+        var yen = 0.0089;
+        var euro = 0.000062;
+        var hasil = 0;
+        Boolean lanjutkan = true;
+        String jawab;
+        double value;
+    }
+}
